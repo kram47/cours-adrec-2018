05e47f757fe798a79505966dad0585a980d8536e
narcomarch <kramoogle@gmail.com>
2019-02-12 16:31:57 +0100

    feat(Holiday): page création de vacances

--- symfony/src/Controller/HolidayController.php
+++ symfony/src/Controller/HolidayController.php
@@ -7,6 +7,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Holiday;
 use App\Repository\HolidayRepository;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class HolidayController extends AbstractController
 {
@@ -26,7 +28,7 @@ class HolidayController extends AbstractController
     }
 
     /**
-        * @Route("/holiday/{id}", name="holiday_show")
+        * @Route("/holiday/{id<\d+>?1}", name="holiday_show")
      */
     public function show(HolidayRepository $repo, $id)
     {
@@ -37,6 +39,47 @@ class HolidayController extends AbstractController
             "holiday" => $holiday
         ]);
     }
+
+    /**
+     * @Route("/holiday/new", name="holiday_create")
+     * @Route("/holiday/{id}/edit", name="holiday_edit")
+     */
+    public function form(Holiday $holiday = null, Request $req, ObjectManager $manager)
+    {
+        if (!$holiday) {
+            $holiday = new Holiday();
+        }
+
+        $form = $this->createFormBuilder($holiday)
+                     ->add("location")
+                     ->add("duration")
+                     ->add("peopleCount")
+                     ->getForm();
+
+        $form->handleRequest($req);
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$holiday->getId()) {
+                // date de creation
+            }
+            else {
+                // date de modification
+            }
+
+            $manager->persist($holiday);
+            $manager->flush();
+
+            return $this->redirectToRoute("holiday_show",
+                ["id" => $holiday->getId()]
+            );
+        }
+
+        return $this->render('holiday/create.html.twig', [
+            "formHoliday" => $form->createView(),
+            "isEditMode" => $holiday->getId() !== null
+        ]);
+    }
+
+
 
 
 }
